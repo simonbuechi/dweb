33114e297e2fcb52e4c020c98c99c5843e6188e9
Simon Buechi <simon.buechi@gmail.com>
2020-09-13 20:51:58 +0200

    remove serviceworker from index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { HashRouter as Router } from "react-router-dom";
 import App from "./App";
 import theme from "./style/theme";
 import "./i18n/i18n";
-import * as serviceWorker from "./serviceWorker";
+//import * as serviceWorker from "./serviceWorker";
 //import bgvideo from "./assets/bgvideo.mp4";
 /*
       <video autoPlay loop muted id="myVideo">
@@ -27,4 +27,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
