2537c60c7cbdae25caf3185cba03e8c645181e11
Simon Buechi <simon.buechi@gmail.com>
2021-01-20 13:30:56 +0100

    include serviceworker

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import theme from "./style/theme";
 import "./i18n/i18n";
 import SplashScreen from "./structure/SplashScreen"
+import * as serviceWorker from "./serviceWorker";
 //lazy load
 const ThemeProvider = lazy(() => import("./utils/ThemeProvider"));
 const Router = lazy(() => import("./utils/Router"));
@@ -21,3 +22,8 @@ ReactDOM.render(
     ,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
