b7b1144dfd36b6e00385aaef8e1fd5d18c191d43
Simon Buechi <simon.buechi@gmail.com>
2021-02-21 15:20:50 +0100

    update A009

--- src/artworks/A009.js
+++ src/artworks/A009.js
@@ -9,20 +9,26 @@ export default (props) => {
   // set size to fullscreen
   const canvasWidth = window.innerWidth;
   const canvasHeight = window.innerHeight;
-  const ITERATIONS_MINIMUM = 300;
-  const ITERATIONS_MULTIPLIER = 1000;
-  const BRUSH_EXTENSION = 1.25; // make sure to fill in edges of canvas
+  const ITERATIONS_MINIMUM = 200;
+  const ITERATIONS_MULTIPLIER = 800;
+  const BRUSH_EXTENSION = 1.2; // make sure to fill in edges of canvas
+  const BRUSH_THICKNESS = 50;
+  const BRUSH_THICKNESS_MAX = 10;
+  const COLOR_VARIANCE = 20;
+  const BACKGROUND_SATURATION = 80;
+  const BACKGROUND_BRIGHTNESS = 50;
+  const BACKGROUND_BRIGHTNESS_LIGHTEN = 30;
 
   let colors = [];
-  let brush = { x: 0, y: 0, px: 0, py: 0 }
-  let seed2;
+  let brush = { x: 0, y: 0, px: 0, py: 0 };
+  let xoff2 = 0;
   let xoff = 0;
   let i = 0;
   let iterations;
 
   function drizzle(p5) {
-    let s = 1 + 30 / p5.dist(brush.px, brush.py, brush.x, brush.y);
-    s = p5.min(15, s);
+    let s = 1 + BRUSH_THICKNESS / p5.dist(brush.px, brush.py, brush.x, brush.y);
+    s = p5.min(BRUSH_THICKNESS_MAX, s);
     p5.strokeWeight(s);
     p5.stroke(0);
     p5.line(brush.px, brush.py, brush.x, brush.y);
@@ -37,9 +43,9 @@ export default (props) => {
     let mx = 10 * p5.movedX;
     let my = 10 * p5.movedY;
     for (let i = 0; i < 80; i++) {
-      seed2 += .01;
-      let x = bx + mx * (0.5 - p5.noise(seed2 + i));
-      let y = by + my * (0.5 - p5.noise(seed2 + 2 * i));
+      xoff2 += 0.01;
+      let x = bx + mx * (0.5 - p5.noise(xoff2 + i));
+      let y = by + my * (0.5 - p5.noise(xoff2 + 2 * i));
       let s = 150 / p5.dist(bx, by, x, y);
       if (s > 20) s = 20;
       let a = 255 - s * 5;
@@ -47,7 +53,7 @@ export default (props) => {
       c.setAlpha(a);
       p5.fill(c);
       p5.ellipse(x, y, s);
-      seed2 += .01;
+      xoff2 += 0.01;
     }
   }
 
@@ -63,64 +69,95 @@ export default (props) => {
   const setup = (p5, canvasParentRef) => {
     // setup canvas
     p5.createCanvas(canvasWidth, canvasHeight).parent(canvasParentRef);
-    p5.noStroke();
-    seed2 = p5.random(1000);
+    // setup noise
+    p5.noiseSeed(seed !== null ? seed : p5.random(1, 1000));
+    p5.noiseDetail(4, 0.5);
+    // setup color scheme
+    p5.colorMode(p5.HSB, 360, 100, 100);
+    const baseHue = p5.floor(p5.noise(1) * 360);
+    colors = [
+      p5.color(baseHue, p5.floor(p5.noise(0.2) * (95 - 70) + 70), 50),
+      p5.color(baseHue + 1 * COLOR_VARIANCE, p5.floor(p5.noise(0.2) * (95 - 70) + 70), 50),
+      p5.color(baseHue + 2 * COLOR_VARIANCE, p5.floor(p5.noise(0.2) * (95 - 70) + 70), 50),
+      p5.color(baseHue + 3 * COLOR_VARIANCE, p5.floor(p5.noise(0.2) * (95 - 70) + 70), 50),
+      p5.color(baseHue + 4 * COLOR_VARIANCE, p5.floor(p5.noise(0.2) * (95 - 70) + 70), 50),
+      p5.color(baseHue + 5 * COLOR_VARIANCE, p5.floor(p5.noise(0.2) * (95 - 70) + 70), 50),
+    ];
+
+    // old color scheme
+    /*
     colors = [
       p5.color(112, 112, 74), //green
       p5.color(245, 198, 110), //yellow
       p5.color(242, 229, 194), //cream
       p5.color(115, 106, 97), //light grey
       p5.color(215, 90, 34), //orange
-      p5.color(235, 61, 0)] // red-orange
-    let base = p5.floor(p5.random(colors.length));
-    p5.background(colors[base]);
-    colors.splice(base, 1);
-    // setup noise
-    p5.noiseSeed(seed !== null ? seed : p5.random(1, 1000));
-    p5.noiseDetail(4, 0.5);
+      p5.color(235, 61, 0), // red-orange
+    ]; 
+    */
 
+    //set background
+    const backgroundHue = p5.floor(p5.noise(10) * 360);
+    p5.background(p5.color(backgroundHue, BACKGROUND_SATURATION, BACKGROUND_BRIGHTNESS));
+    p5.noStroke();
+    /*
+    // linear gradient
+    p5.noFill();
+    for (var y = 0; y < p5.height; y++) {
+      var inter = p5.map(y, 0, p5.height, 0, 1);
+      var c = p5.lerpColor(colors[base], colors[2], inter);
+      p5.stroke(c);
+      p5.line(0, y, p5.width, y);
+    }
+    */
+    // radial gradient
+    for (let i = Math.max(p5.width, p5.height); i > 0; i--) {
+      const step = i / Math.max(p5.width, p5.height);
+      const gradient = p5.lerpColor(
+        p5.color(backgroundHue, BACKGROUND_SATURATION, BACKGROUND_BRIGHTNESS + BACKGROUND_BRIGHTNESS_LIGHTEN),
+        p5.color(backgroundHue, BACKGROUND_SATURATION, BACKGROUND_BRIGHTNESS),
+        step
+      );
+      p5.fill(gradient);
+      p5.ellipse(p5.width / 2, p5.height / 2, step * p5.width, step * p5.height);
+    }
     // set start point
     brush.x = 400;
     brush.y = 400;
     brush.px = brush.x;
     brush.py = brush.y;
-
     //set no of iterations
-    iterations = p5.noise(10) * ITERATIONS_MULTIPLIER + ITERATIONS_MINIMUM;
+    iterations = p5.noise(100) * ITERATIONS_MULTIPLIER + ITERATIONS_MINIMUM;
   };
 
   const draw = (p5) => {
     // set brush location
     brush.x = p5.width * BRUSH_EXTENSION * p5.noise(xoff);
-    brush.y = p5.height * BRUSH_EXTENSION * p5.noise(xoff+5);  
-
+    brush.y = p5.height * BRUSH_EXTENSION * p5.noise(xoff + 5);
     if (p5.frameCount > 10) {
       drizzle(p5);
     }
     brush.px = brush.x;
     brush.py = brush.y;
-
     if (p5.frameCount % 30 === 0) {
       splatter(p5, brush.x, brush.y);
       splatter(p5, p5.width - brush.x, p5.height - brush.y);
       stipple(p5, brush.x, brush.y, 0);
       stipple(p5, p5.width - brush.x, p5.height - brush.y, 255);
     }
-
     xoff = xoff + 0.01;
     i = i + 1;
     if (i > iterations) {
-      p5.noLoop(); 
+      p5.noLoop();
     }
   };
 
-  const keyPressed = (p5) => { //save the framme when we press the letter s
-
-    if (p5.keyCode === p5.DOWN_ARROW) {
-      p5.saveFrame("artwork-###.png");
-    } 
-
-  }
+  const keyPressed = (p5) => {
+    //save the canvas when press "s" or space
+    if (p5.keyCode === 83 || p5.keyCode === 32) {
+      p5.saveCanvas("simons_artwork", "jpg");
+    }
+  };
 
   return <Sketch setup={setup} draw={draw} keyPressed={keyPressed} />;
 };
