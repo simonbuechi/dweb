253d218d17e7f782b6a48c63ede51bdad1e899d0
Simon Buechi <simon.buechi@gmail.com>
2021-08-10 16:11:00 +0200

    update artwork A023

--- src/artworks/A023.js
+++ src/artworks/A023.js
@@ -6,16 +6,12 @@ export default (props) => {
   const CANVAS_WIDTH = window.localStorage.getItem("customWidth") ? window.localStorage.getItem("customWidth") : window.innerWidth;
   const CANVAS_HEIGHT = window.localStorage.getItem("customHeight") ? window.localStorage.getItem("customHeight") : window.innerHeight;
   const SEED = window.localStorage.getItem("signature");
-
-  let bottomEnd = 0.48;
-  let topEnd = 0.52;
-  let size = 1100;
-  let numParticles = 4000;
-  let numSets = 5;
+  const STROKE_WEIGHT = 1;
+  const BOTTOM_END = 0.48;
+  const TOP_END = 0.52;
+  const NUMBER_PARTICLES = 4000;
+  let numSets;
   let particle_sets = [];
-  let hu = 0;
-  let perlinSeedX;
-  let perlinSeedY;
 
   class Particle {
     constructor(p5, x, y, phi) {
@@ -30,26 +26,22 @@ export default (props) => {
     update(p5, index) {
       this.pos.x += p5.cos(this.angle);
       this.pos.y += p5.sin(this.angle);
-      let nx = 1.8 * scaleFunction(this.pos.x);
-      let ny = 1.8 * scaleFunction(this.pos.y);
+      let nx = 1.8 * (p5.width - this.pos.x / p5.width) * 2;
+      let ny = 1.8 * (p5.height - this.pos.y / p5.height) * 2;
       let n = {
         x: nx,
         y: ny
       };
-      let nval = (p5.noise(n.x + perlinSeedX, n.y - perlinSeedY) + 0.045 * (index - numSets / 2)) % 1;
+      let nval = (p5.noise(n.x + 10, n.y - 10) + 0.045 * (index - numSets / 2)) % 1;
       this.angle += 3 * (nval * 2 - 1);
       this.val = nval;
     }
   
     display(p5, index) {
-      if (this.val > bottomEnd && this.val < topEnd) {
+      if (this.val > BOTTOM_END && this.val < TOP_END) {
         p5.rect(this.pos.x, this.pos.y, 1, 1);
       }
     }
-  }
-  
-  function scaleFunction(n) {
-    return (size - n / size) * 2 - 1;
   }
 
   const setup = (p5, canvasParentRef) => {
@@ -57,27 +49,22 @@ export default (props) => {
     p5.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT).parent(canvasParentRef);
     p5.noiseSeed(SEED !== null ? SEED : p5.floor(p5.random(1, 10000)));
     p5.randomSeed(SEED !== null ? SEED : p5.floor(p5.random(1, 10000)));
-
-    perlinSeedX = p5.round(p5.random() * size);
-    perlinSeedY = p5.round(p5.random() * size);
-
     p5.noFill();
-    p5.background('#e7e7db');
     p5.colorMode(p5.HSB, 255);
-    hu = p5.random(255);
-    p5.stroke(hu, 150, 100, 10);
-    p5.strokeWeight(0.7);
-    p5.noFill();
+    p5.background(30, 40, 210, 100);
+    p5.stroke(p5.random(255), 150, 100, 10);
+    p5.strokeWeight(STROKE_WEIGHT);
     p5.smooth();
+    //numSets = p5.max(p5.floor(p5.randomGaussian(7,4)),3);
+    numSets = 5;
     for (var j = 0; j < numSets; j++) {
       let particlesArray = [];
-      
-      for (var i = 0; i < numParticles; i++) {
+      for (var i = 0; i < NUMBER_PARTICLES; i++) {
         particlesArray.push(
           new Particle(
             p5,
-            p5.randomGaussian(size / 2, 110),
-            p5.randomGaussian(size / 2, 110),
+            p5.randomGaussian(p5.width / 2, 110),
+            p5.randomGaussian(p5.height / 2, 110),
             p5.random() * 2 * p5.PI
           )
         );
